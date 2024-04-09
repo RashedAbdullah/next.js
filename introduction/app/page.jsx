62d514c0a201e1819ee27fa949c4e5444cb372ac
RashedAbdullah <maarashed40@gmail.com>
2024-04-10 00:07:53 +0600

    practices

--- introduction/app/page.jsx
+++ introduction/app/page.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+import Image from "next/image"
 const Home = async () => {
   const url = "https://jsonplaceholder.typicode.com/users";
   const response = await fetch(url, { cache: "force-cache" });
   if (!response.ok) throw new Error("There was an error occured");
   const data = await response.json();
-  console.log(data);
+
   return (
     <div className=" p-5 grid grid-cols-3">
       {data.map((user) => (
-        <div key={user.id} className="m-2 bg-gray-200 p-3 rounded">
+        <div key={user.id} className="m-2 bg-green-400 text-blue-950 p-3 rounded relative shadow-xl">
           <p className="font-thin text-sm">User name: {user.username}</p>
           <h2>Name: {user.name}</h2>
           <p className="text-sm">E-mail: {user.email}</p>
           <p>
             Address: {user.address.street}, {user.address.city}
           </p>
+          <Image  src="/hero.svg" alt="" width={5000} height={5000} className="absolute top-0"/>
         </div>
       ))}
     </div>
