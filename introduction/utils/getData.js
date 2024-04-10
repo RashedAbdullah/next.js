6e8e64b9441d3eab871e3dd02fa55693d513f776
RashedAbdullah <maarashed40@gmail.com>
2024-04-11 00:25:55 +0600

    revalidate

--- introduction/utils/getData.js
+++ introduction/utils/getData.js
@@ -1,5 +1,10 @@
 export const getData = async (url) => {
-  const data = await fetch(url, { cache: "no-store" });
+  // const data = await fetch(url, { cache: "no-store" });
+  const data = await fetch(url, {
+    next: {
+      revalidate: 10,
+    },
+  });
   if (!data.ok) {
     throw new Error("Failed Data fetching...");
   }
