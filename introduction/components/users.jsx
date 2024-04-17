281e17fce0b35947b2cd5adff64998527c8bd04e
RashedAbdullah <maarashed40@gmail.com>
2024-04-17 10:36:33 +0600

    users styled with grid

--- introduction/components/users.jsx
+++ introduction/components/users.jsx
@@ -5,10 +5,10 @@ const Users = async () => {
   const users = await getUsers();
 
   return (
-    <div className="w-full p-10 flex gap-5 flex-wrap">
+    <div className="grid grid-cols-4 gap-4">
       {users.length ? (
         users.reverse().map((user, inx) => (
-          <div key={user._id} className="bg-gray-200 py-5 px-10 rounded-lg">
+          <div key={user._id} className="bg-gray-200 py-5 px-10 rounded-lg shadow-lg hover:bg-gray-100 transition duration-300 cursor-pointer">
             <div className="text-center">{inx + 1}</div>
             <h2>Name: {user.name}</h2>
             <p>Address: {user.address}</p>
