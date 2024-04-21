2e5cead931932367f09cc3a28e8dc59f1a402f5c
RashedAbdullah <maarashed40@gmail.com>
2024-04-21 18:25:28 +0600

    image optimaization

--- introduction/next.config.mjs
+++ introduction/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent.fcgp3-2.fna.fbcdn.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
