37e52f26a81c370fb0e370b74e07056b9bf6e8d0
RashedAbdullah <maarashed40@gmail.com>
2024-05-10 11:07:42 +0600

    Next.js typescript learing

---
+++ typescript/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet eius eveniet aperiam aliquam neque corrupti alias cumque dolor, dicta consectetur totam aliquid. Recusandae itaque expedita nihil, adipisci quo tempore.
+    </main>
+  );
+}
