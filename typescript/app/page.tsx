c3bbc882b92ece8e8ac4477f53f3f6a195057e52
RashedAbdullah <maarashed40@gmail.com>
2024-05-11 12:46:23 +0600

    Nextjs auth and typescript

--- typescript/app/page.tsx
+++ typescript/app/page.tsx
@@ -1,6 +1,8 @@
 
 
 export default function Home() {
+
+  const count:boolean = false
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet eius eveniet aperiam aliquam neque corrupti alias cumque dolor, dicta consectetur totam aliquid. Recusandae itaque expedita nihil, adipisci quo tempore.
